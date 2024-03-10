2938e01945e5a1ac7672b2eccb40142f9c111346
Annelies11 <mmahmudi1103@gmail.com>
2024-03-10 20:56:28 +0700

    create order

--- api/routes/product.js
+++ api/routes/product.js
@@ -8,9 +8,35 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-    res.status(200).json({
+    res.status(201).json({
         message : "Handling POST request to /product"
     });
 });
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if (id === 'aris'){
+        res.status(200).json({
+            message: 'Hello bro',
+            id: id
+        })
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        })
+    }
+})
+
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: "Updated Product"
+    })
+})
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: "Deleted Product"
+    })
+})
 
 module.exports = router;
