cb2a3d2c5b30be435ec7f0525fdcb0c8bdf7d456
Annelies11 <mmahmudi1103@gmail.com>
2024-03-10 19:57:57 +0700

    second commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+const productRoutes = require('./api/routes/product')
+
+app.use('/product', productRoutes);
+
+module.exports = app;
+
